0de39772002d7e6565777fbb955752352d7ee594
pratap dorjee <pratapdorjee515@gmail.com>
2021-03-14 11:57:17 +0530

    2nd commit tasks tracker

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { useState } from 'react'
 import Header from './componets/Header'
 import Tasks from './componets/Tasks'
-
+import AddTask from './componets/AddTask'
 
 const App=()=>{
+  const [showAddTask,setShowAddTask]=useState(false)
   const [tasks ,setTasks]= useState([
     {
     id: 1,
@@ -16,16 +17,30 @@ const App=()=>{
     day: 'march 3',
     reminder: true,
 }])
+  
+    //add task
+    const addTask=(task)=>{
+      const id=Math.floor(Math.random()*1000)+1
+      const newTask={id,...task }
+      setTasks([...tasks,newTask])
+    }
+
 
   //delete task
   const deleteTasks=(id)=>{
     setTasks(tasks.filter((task)=>task.id!==id))
   }
 
+   // toggle reminder
+   const toggleReminder=(id)=>{
+    setTasks(tasks.map((task)=>task.id===id ? {...task,reminder: !task.reminder}:task))
+   }
+
   return (
     <div className="container">
-      <Header/>
-      <Tasks tasks={tasks} onDelete={deleteTasks}/>
+      <Header onAdd={()=>setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask}/>}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTasks} onToggle={toggleReminder}/> : ('No Tasks to Show')}
     </div>
   );  
 }
--- src/componets/Header.js
+++ src/componets/Header.js
@@ -2,14 +2,12 @@ import PropTypes from 'prop-types'
 
 import Button from './Button'
 
-const Header = ({ title }) => {
-const onClick=()=>{
-    console.log('click')
-    }
+const Header = ({ title,onAdd,showAdd}) => {
+
    return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button onClick={onClick} color='green' text='ADD'/>
+            <Button onClick={onAdd} color={showAdd ? 'red':'green'} text={showAdd ? 'Close':'Add' }/>
             </header>
     )
 }
